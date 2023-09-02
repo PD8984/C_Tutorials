b439cd2085993fc5ab9ddd3b2c2483ed27bc3821
PD8984 <preetdhiman85720@gmail.com>
2023-09-03 01:01:35 +0530

    Control Flow Questions

--- 1-Introduction/1-Hello-World/main.c
+++
@@ -1,9 +0,0 @@
-// Preprocessor Decralative
-#include <stdio.h>
-
-// function main begins program execution
-// The keyword int to left of main indicates the main "returns" an integer value
-// The void in parentheses means that main does't recieve any information.
-int main(void) { 
-	printf("Hello World\n");
-} // End of function main
